a63f2d7a43de083a085b34ea38fd280741b94a9c
Hritik Raj <73677045+Hritik003@users.noreply.github.com>
2023-10-30 23:05:56 +0530

    minted 10000 KT tokens to krypt Company

---
+++ KT.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC20/ERC20.sol";
+
+//inheriting from another contract named ERC20. 
+contract kryptToken is ERC20 {
+    address public  admin;
+   
+    constructor() ERC20("kryptToken", "KT") {
+        admin = msg.sender;
+        _mint(admin, 10000*10**18); //10000 KT tokens to admin
+    }
+
+}
